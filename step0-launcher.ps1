22293f0a2b052a971c1d3f1aa157096e81c61ac2
thuguerre <huguerre.thomas@gmail.com>
2019-11-02 20:41:12 +0100

    correcting bug while performing test on step3: moved folders to unknown folder are not deleted when deleting files from GIT control. They cause errors on the following execution. This correction adds a new preparation step 0.1 deleting empty folders if asked by user (not by default to prevent accidental and definitive deletion in original folders and files)

--- step0-launcher.ps1
+++ step0-launcher.ps1
@@ -8,15 +8,28 @@
 .PARAMETER logFile
     Path of the file where to log all this script's actions.
     By default, ".\takeout-googlephotos-cleaner.log" in root folder, ignored by GIT.
+.PARAMETER emptyWorkFolder
+    Delete definitively empty folders in work folders, used during test.
+    By default, set to 0, not to delete accidentaly files from original folder.
+    Set to 1 if you want to empty work folders.
 #>
 
-param(  [string]$takeOutFolderPath = ".\test-resources\test-folder",
-        [string]$referenceFolderPath = ".\test-resources\reference-folder",
-        [string]$unknownFolderPath = ".\test-resources\unknown",
-        [string]$logFile = ".\takeout-googlephotos-cleaner.log")
+param(  [string] $takeOutFolderPath = ".\test-resources\test-folder",
+        [string] $referenceFolderPath = ".\test-resources\reference-folder",
+        [string] $unknownFolderPath = ".\test-resources\unknown",
+        [string] $logFile = ".\takeout-googlephotos-cleaner.log",
+        [bool] $emptyWorkFolders = 0)
 
 
 Clear-Content $logFile
+
+if ( $emptyWorkFolders ) {
+
+    # Step 0.1 : prepare work folders by deleting all empty folders from previous tests
+    $step01Path = ".\step0.1-clean-empty-folders-in-unknown.ps1 -targetFolder """ + $unknownFolderPath + """ -logFile """ + $logFile + """"
+    Invoke-Expression $step01Path
+}
+
 
 # Step 1 : remove all JSON files from target folder
 $step1Path = ".\step1-clean-json-files.ps1 -targetFolder """ + $takeOutFolderPath + """ -logFile """ + $logFile + """"
