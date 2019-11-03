0c94577d6757ce39f3402dc9e1c656e3a451672c
thuguerre <huguerre.thomas@gmail.com>
2019-11-03 20:14:00 +0100

    all existing files on both sides are now well deleted empty folders are deleted too. only files to synchronize are now in take-out-archive

--- step4-deleting-existing-files.ps1
+++ step4-deleting-existing-files.ps1
@@ -26,12 +26,39 @@ Add-content $Logfile -value "#####################################"
 Add-content $Logfile -value ""
 
 
-$takeOutFolder = Get-ChildItem -Recurse -Path $takeOutArchivePath
-$referenceFolder = Get-ChildItem -Recurse -Path $referenceFolderPath
+# getting both complete folder structures, only for files
+$takeOutFolder = Get-ChildItem -Recurse -Path $takeOutArchivePath | Where-Object { $_.PsIsContainer -eq $false }
+$referenceFolder = Get-ChildItem -Recurse -Path $referenceFolderPath | Where-Object { $_.PsIsContainer -eq $false }
 
-Compare-Object -ReferenceObject $referenceFolder -DifferenceObject $takeOutFolder -IncludeEqual -ExcludeDifferent -Passthru `
-    | % {$_.FullName}
+# comparing both file lists, identifying what ones are existing on both sides and deleting them
+Compare-Object -ReferenceObject $takeOutFolder -DifferenceObject $referenceFolder -IncludeEqual -ExcludeDifferent `
+    | ForEach-Object {
+        Add-Content $logFile -Value $_.InputObject.FullName
+        Remove-Item -Path $_.InputObject.FullName
+    }
 
+
+Add-content $Logfile -value ""
+Add-content $Logfile -value "# Deleting empty folders"
+Add-content $Logfile -value ""
+
+Do {
+
+    #selecting folders to delete
+    $allEmptyFolders = (Get-ChildItem $takeOutArchivePath -Recurse `
+        | Where-Object { $_.PsIsContainer -eq $true } `
+        | Where-Object { $_.GetFiles().Count -eq 0 -and $_.GetDirectories().Count -eq 0 } ).Fullname
+
+    #logging folders to delete
+    $allEmptyFolders | Add-Content $logFile
+
+    #deleting empty folders
+    if ( $allEmptyFolders.Count -gt 0 ) {
+        $allEmptyFolders | Remove-Item
+    }
+
+} While ( $allEmptyFolders.Count -gt 0 )
+    
 
 Add-content $Logfile -value ""
 Add-content $Logfile -value ""
